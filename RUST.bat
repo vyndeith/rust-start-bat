d7d3280b37f41300e4a696cf327e1ae0f1f5a322
vyndeith <6996satoru@gmail.com>
2026-04-05 13:18:56 +0300

    Added new features
    Added new test commands with threads. Useful premium server prioritizing disabling, and another test command that decide to use all available vram for texture rendering

--- RUST.bat
+++ RUST.bat
@@ -23,10 +23,19 @@ for /f "tokens=*" %%L in ('type "%STEAM%\steamapps\libraryfolders.vdf" ^| findst
 if defined RUST (
     start "" "%RUST%" ^
         -steam ^
-        -high ^
+        -high 
+
         -gc.incremental_enabled 5 ^
+        -lerp.timeoffsetinterval 4 ^
+        -texture.memory_vram_factor 2 ^
+        -ai.maxgroundaligndist 1 ^
+        -instruments.processsustainpedal 0 
+
+        -global.maxthreads 14
+        -grass.maxthreads 2 ^
         -graphics.branding 0 ^
-        -headlerp 5 ^
+        -headlerp 5 
+
         -decal.capacity 0 ^
         -decal.limit 0 ^
         -decal.instancing 0 ^
@@ -35,8 +44,15 @@ if defined RUST (
         -player.eye_blinking 0 ^
         -player.eye_movement 0 ^
         -legs.enablelegs 0 ^
+        -particle.ik 0 
+
         -playercull.enabled 0 ^
-        -culling.toggle 1
+        -culling.toggle 1 ^
+        -console.erroroverlay 0 ^
+        -gametip.showgametips 0 
+
+        -client.prioritize_premium_servers 0 ^
+        -client.cached_browser_parallel 1 
 ) else (
     echo Rust.exe not found
     pause
