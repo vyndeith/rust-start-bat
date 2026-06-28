c22943fb34b457c793c445ea4efeba54151e0ad9
vyndeith <6996satoru@gmail.com>
2026-06-28 04:47:20 +0200

    useless shii
    im daun btw

--- RUST.bat
+++ RUST.bat
@@ -23,7 +23,7 @@ for /f tokens^=2^,4^ delims^=^" %%a in ('type "%STEAM%\steamapps\libraryfolders.
 )
 ::================================
 :: запуск самого лаунчера
-:: launcher starting
+:: launcher start
 if defined RUST (
     start "" "%RUST%" ^
         -steam ^
@@ -31,27 +31,28 @@ if defined RUST (
         -client.headlerp 1000 ^
 
         -gc.incremental_milliseconds 1 ^
-        -lerp.timeoffsetinterval 4 ^
         -texture.memory_vram_factor 2 ^
-        -ai.maxgroundaligndist 1 ^
-        -instruments.processsustainpedal 0 ^
-
+        -graphics.maxqueuedframes 0 ^
         -global.maxthreads %MAXTHREADS% ^
-        -grass.maxthreads 2 ^
-        -graphics.branding 0 ^
+        -grass.maxthreads 1 ^
+        -grass.refresh_budget 0.1 ^
 
         -decal.capacity 0 ^
         -decal.limit 8 ^
         -decal.cache 0 ^
         -decal.instancing 1 ^
+
         -effects.maxgibs 0 ^
         -effects.maxgibdist 0 ^
         -effects.maxgiblife 0 ^
+
         -player.cold_breath 0 ^
         -player.eye_blinking 0 ^
         -player.eye_movement 0 ^
         -legs.enablelegs 0 ^
         -particle.ik 0 ^
+        -ai.maxgroundaligndist 1 ^
+        -npcwalkanimation.framebudgetms 0 ^
 
         -playercull.enabled 0 ^
         -culling.toggle 1 ^
@@ -59,25 +60,18 @@ if defined RUST (
         -culling.entityupdaterate 3 ^
         -culling.entityminshadowculldist 2 ^
         -culling.envmindist 5 
-        -console.erroroverlay 0 ^
-        -gametip.showgametips 0 ^
-
-        -client.prioritize_premium_servers 0 ^
-        -client.cached_browser_parallel 0 ^
-
-        -effects.otherplayerslightflares 0 ^
 
         -render.building_blocked_preview_distance 50 ^
 	    -nobuildzonematerialcontroller.setstrengthday 0 ^
 	    -nobuildzonematerialcontroller.setstrengthnight 0 ^
 	    -nobuildzonematerialcontroller.setheight 10 ^
 
-        -audio.framebudget 0.2 ^
-        -audio.minupdatefraction 0.05 ^
-        -graphics.maxqueuedframes 0 ^
-        -grass.refresh_budget 0.1 ^
-        -npcwalkanimation.framebudgetms 0 ^
-        -signage.texturerequestdistance 20 
+        -graphics.branding 0 ^
+        -console.erroroverlay 0 ^
+        -gametip.showgametips 0 ^
+        -client.cached_browser_parallel 0 ^
+        -signage.texturerequestdistance 20 ^
+        -instruments.processsustainpedal 0 
 ) else (
     echo Rust.exe not found
     pause
